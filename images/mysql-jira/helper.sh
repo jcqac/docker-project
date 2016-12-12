8eb36773148524107a3e9ac387cfe16b546ed75f
jcqac <johnny.che@qa.com>
2016-12-12 22:04:58 +0000

    22nd commit of Docker Project
    Java, Jenkins, Jira, MySQL, Nexus, Postfix, Zabbix and Urvancode Deploy are now working
    
    ToDo: update gitignore
    
    Signed-off-by: jcqac <johnny.che@qa.com>

--- images/mysql-jira/helper.sh
+++ images/mysql-jira/helper.sh
@@ -5,5 +5,5 @@ chgrp mysql /var/lib/mysql
 service mysql start
 
 if [ ! -d /var/lib/mysql/jiradb ]; then
-	mysql -sfu root --password=root < /opt/mysql/tmp/post_install.sql
+	mysql -sfu root --password=root < "/opt/mysql/tmp/post_install.sql"
 fi
